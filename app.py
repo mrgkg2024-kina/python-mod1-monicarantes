27cc1ca6c241c60ba86e7f98a49b69dfb1f46a9a
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-07-17 19:57:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import streamlit as st
 
 
 st.session_state
-st.title("Proyecto Aplicado en Streamlit – Fundamentos de Programación")
+st.title("Módulo 1 – Fundamentos de Programación utilizando Python y Streamlit")
 st.sidebar.title("Menú")
 
 modulo =st.sidebar.selectbox("Elija un módulo",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
 
 if modulo == "Home":
 
-    st.header("Módulo Home – Presentación del Proyecto")
+    st.header("Presentación del Proyecto")
 
     st.write("Título del proyecto            : Módulo 1 – Python Fundamentals")
     st.write("Nombre completo del estudiante : Mónica Rantes García")
