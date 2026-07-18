20a5dd8cdcca74b16bb7d566f97b2eadf2ab7110
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-07-17 19:52:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,13 @@ if modulo == "Home":
 
     st.header("Módulo Home – Presentación del Proyecto")
 
-    st.info(
-        "Título del proyecto            : Módulo 1 – Python Fundamentals"
-        "Nombre completo del estudiante : Mónica Rantes García"
+    st.write("Título del proyecto            : Módulo 1 – Python Fundamentals")
+    st.write("Nombre completo del estudiante : Mónica Rantes García")
+    st.write("Nombre del curso o módulo: Especialización en Python for Analytics")
+    st.write("Año :2026")
+    st.write("Breve descripción del objetivo del trabajo : Desarrollar una aplicación interactiva en Streamlit que integre los conceptos 
+            fundamentales aprendidos durante el Módulo 1 del curso, incluyendo variables,estructuras de datos, control de flujo, funciones, 
+            programación funcional y programación orientada a objetos (POO). ")
+    st.write("Lista de tecnologías utilizadas: Python, Streamlit, NumPy")        
+    
     )
