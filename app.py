f3d05f40edad1209eb441a3a0ccd4b0d7b346b66
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-07-17 19:22:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+
+st.session_state
+st.title("Proyecto Aplicado en Streamlit – Fundamentos de Programación")
+st.sidebar.title("Menú")
+
+modulo =st.sidebar.selectbox("Elija un módulo",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
