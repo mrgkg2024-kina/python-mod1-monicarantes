c1feb495b668f156d494675d718c359d92932707
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-07-25 22:40:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -279,6 +279,12 @@ elif modulo == "Ejercicio 4":
             else:
                 st.error("Ingresa nombre del servidor")
 
+    if st.button("🗑️ Limpiar"):
+        st.session_state.servidores,st.session_state.tiempo_total,st.session_state.tiempo_caida,st.session_state.almacenamiento_total,
+        st.session_state.almacenamiento_usado =[]
+        st.session_state.clear_inputs = True
+        st.rerun()      
+    
     if st.session_state.servidores:
         st.write(st.session_state.get("ultimo_resumen"))
         df = pd.DataFrame({
@@ -290,11 +296,7 @@ elif modulo == "Ejercicio 4":
         })
         st.dataframe(df, use_container_width=True, hide_index=True)         
     
-    if st.button("🗑️ Limpiar"):
-        st.session_state.servidores,st.session_state.tiempo_total,st.session_state.tiempo_caida,st.session_state.almacenamiento_total,
-        st.session_state.almacenamiento_usado =[]
-        st.session_state.clear_inputs = True
-        st.rerun()      
+  
          
 
 
