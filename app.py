7244704c7c586e09649bdcef427d585978ab8e28
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-07-26 18:09:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ elif modulo == "Ejercicio 2":
 
 elif modulo == "Ejercicio 3":
     st.subheader("Ejercicio 3 – Uso de funciones desde una librería externa ")
-    st.markdown("""Se utilizará la función calcular_disponibilidad_sistema del archivo de funciones libreria_funciones_proyecto1.py ,
+    st.markdown("""En este ejercicio se utilizará la función calcular_disponibilidad_sistema del archivo de funciones libreria_funciones_proyecto1.py ,
                 luego de ingresar los parámetros requeridos, se ejecutará la función indicada y se mostrarán los resultados""")
                
     st.info("Calcular disponibilidad del sistema")
