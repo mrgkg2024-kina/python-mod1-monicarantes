2a9806b45664ccf51d33cb54120bbc7b45365574
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-07-26 13:39:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -327,6 +327,8 @@ elif modulo == "Ejercicio 4":
                     )
                     st.session_state["ultimo_resumen"] = srv.resumen()
                     st.session_state.dispo_pct[i]=st.session_state["ultimo_resumen"]["disponibilidad_pct"]
+                    st.session_state.clear_inputs = True
+                    st.rerun() 
                      
                  except Exception as e:
                     st.warning(f"No se pudo recalcular el resumen: {e}")
