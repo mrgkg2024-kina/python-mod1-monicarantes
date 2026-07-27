4f4166c30939c04bc275c2c6ecab9e1a686a545f
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-07-26 19:32:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,17 +36,17 @@ if modulo == "Home":
     <h3 style="margin-top:0;">Datos del proyecto</h3>
     <ul style="line-height:1.6; padding-left:18px;">
       <li><strong>Nombre completo del estudiante:</strong> Mónica Rantes García</li>
-      <li><strong>Nombre del curso o módulo:</strong> Especialización en Python for Analytics</li>
+      <li><strong>Nombre del módulo:</strong> Módulo 1 – Python Fundamentals</li>
       <li><strong>Año:</strong> 2026</li>
     </ul>
     </section>
 
     <section style="background:#ffffff; border:1px solid #e5e7eb; border-radius:12px; padding:20px; margin-top:12px;">
-    <h3 style="margin-top:0;">Objetivo</h3>
+    <h3 style="margin-top:0;">Descripción del proyecto</h3>
     <p>
-      Desarrollar una aplicación interactiva en Streamlit que integre los conceptos fundamentales
+      Se desarrolla una aplicación interactiva en Streamlit que integra los conceptos fundamentales
       aprendidos durante el Módulo 1 del curso, incluyendo variables, estructuras de datos, control
-      de flujo, funciones, programación funcional y programación orientada a objetos (POO).
+      de flujo, funciones, programación funcional, programación orientada a objetos (POO) y  operaciones básicas tipo CRUD.
     </p>
     </section>
 
